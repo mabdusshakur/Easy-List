0acd12f68e9c83719341527b6ef13e0aaba3bce9
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-22 15:45:07 +0600

    pin_login implemented on controller

--- app/Http/Controllers/EnterPageController.php
+++ app/Http/Controllers/EnterPageController.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EnterPageRequest;
 use Illuminate\Http\Request;
 
-class EnterPageController extends Controller
+class EnterPageController extends BaseController
 {
     function index()
     {
         return view('EnterPage');
     }
 
-    function enter()
+    function enter(EnterPageRequest $req)
     {
+        try {
+            $data = [
+                'user_name' => $req->user_name,
+                'pin_code' => $req->pin_code
+            ];
+            $res = $this->login($data);
 
+            if ($res != true) {
+                dd($res);
+            }
+
+            return $this->user();
+
+        } catch (\Throwable $th) {
+            dd($th->getMessage());
+        }
     }
 }
