f1f448833e48eccad0c494ad1fe765da905ee517
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-22 15:33:41 +0600

    enter get & post route defined

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
+use App\Http\Controllers\EnterPageController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/enter', [EnterPageController::class, 'index'])->name('enter.index');
+Route::get('/enter', [EnterPageController::class, 'enter'])->name('enter.post');
