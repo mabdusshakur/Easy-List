ebb938e6d5bf8755c9b9d9056807f7915f1a856f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-22 17:09:20 +0600

    user relation added

--- app/Models/ListItem.php
+++ app/Models/ListItem.php
@@ -10,4 +10,9 @@ class ListItem extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'status', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
