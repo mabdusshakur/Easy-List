5b5f32fa02311f4d55dc0d529974022ba40bed05
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-22 22:00:29 +0600

    update item system added

--- resources/views/components/lists/listItem.blade.php
+++ resources/views/components/lists/listItem.blade.php
@@ -1,9 +1,9 @@
 <div>
     <h3>{{ $item->title }}</h3>
     <p>{{ $item->status }}</p>
-    <form action="{{ url('/') }}" method="post">
+    <form action="{{ route('list.patch', $item->id) }}" method="post">
         @csrf
         @method('patch')
-        <button type="submit" value="completed">Completed</button>
+        <button type="submit">{{ $item->status == 'pending' ? 'Mark as Done' : 'Mark as Pending' }}</button>
     </form>
 </div>
