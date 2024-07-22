2e9ccad27d578da476f846defdf001c5199fe246
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-22 15:34:03 +0600

    route method get to post switched [fix]

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\EnterPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/enter', [EnterPageController::class, 'index'])->name('enter.index');
-Route::get('/enter', [EnterPageController::class, 'enter'])->name('enter.post');
+Route::post('/enter', [EnterPageController::class, 'enter'])->name('enter.post');
