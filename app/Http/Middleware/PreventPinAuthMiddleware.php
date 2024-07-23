2517baf921b9f8b35a3caa0c86fd6088202db1fa
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-23 10:14:22 +0600

    prevent auth middleware added

---
+++ app/Http/Middleware/PreventPinAuthMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Controllers\BaseController;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PreventPinAuthMiddleware extends BaseController
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($this->user()['status'] == true) {
+            return redirect()->route('list.index');
+        }
+        return $next($request);
+    }
+}
