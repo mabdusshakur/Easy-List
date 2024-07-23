d3a28e159ae3510bf9222bb0f61fda8a77b6a116
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-23 10:07:44 +0600

    pinAuth middleware implemented on list. app-pages

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ Route::get('/logout', [EnterPageController::class, 'exit'])->name('enter.logout'
 Route::post('/enter', [EnterPageController::class, 'enter'])->name('enter.post');
 
 
-Route::get('/{status?}', [ListController::class, 'index'])->name('list.index');
-Route::post('/', [ListController::class, 'store'])->name('list.post');
-Route::patch('/{listItem}', [ListController::class, 'update'])->name('list.patch');
-Route::delete('/{listItem}', [ListController::class, 'destroy'])->name('list.delete');
+Route::group(['middleware' => ['pinAuth']], function () {
+    Route::get('/{status?}', [ListController::class, 'index'])->name('list.index');
+    Route::post('/', [ListController::class, 'store'])->name('list.post');
+    Route::patch('/{listItem}', [ListController::class, 'update'])->name('list.patch');
+    Route::delete('/{listItem}', [ListController::class, 'destroy'])->name('list.delete');
+});
