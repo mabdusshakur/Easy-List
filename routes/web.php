2abdc18af6883cb4a12272dfa405ce3eb60a0292
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-23 10:14:58 +0600

    routes re grouped with all the middlewares

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,16 @@ use App\Http\Controllers\EnterPageController;
 use App\Http\Controllers\Lists\ListController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/enter', [EnterPageController::class, 'index'])->name('enter.index');
-Route::get('/logout', [EnterPageController::class, 'exit'])->name('enter.logout');
-Route::post('/enter', [EnterPageController::class, 'enter'])->name('enter.post');
-
+Route::group(['middleware' => ['preventPinAuth']], function () {
+    Route::get('/enter', [EnterPageController::class, 'index'])->name('enter.index');
+    Route::post('/enter', [EnterPageController::class, 'enter'])->name('enter.post');
+});
 
 Route::group(['middleware' => ['pinAuth']], function () {
     Route::get('/{status?}', [ListController::class, 'index'])->name('list.index');
     Route::post('/', [ListController::class, 'store'])->name('list.post');
     Route::patch('/{listItem}', [ListController::class, 'update'])->name('list.patch');
     Route::delete('/{listItem}', [ListController::class, 'destroy'])->name('list.delete');
+
+    Route::get('/logout', [EnterPageController::class, 'exit'])->name('enter.logout');
 });
