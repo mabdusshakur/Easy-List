644ccabd9f57cfdde7d83a2c58b4c44ffc3965d0
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-07-24 14:45:47 +0600

    select fixed

--- resources/views/components/lists/drop-down.blade.php
+++ resources/views/components/lists/drop-down.blade.php
@@ -1,9 +1,8 @@
 <div class="flex justify-center">
     <select class="mt-2 rounded-md border bg-transparent px-20 py-2" id="status" name="status" onchange="updateStatus()">
-        <option>Filter</option>
-        <option value="all">all</option>
-        <option value="completed">completed</option>
-        <option value="pending">pending</option>
+        <option value="all" {{ request('status') == 'all' ? 'selected' : '' }}>all</option>
+        <option value="completed" {{ request('status') == 'completed' ? 'selected' : '' }}>completed</option>
+        <option value="pending" {{ request('status') == 'pending' ? 'selected' : '' }}>pending</option>
     </select>
 </div>
 
